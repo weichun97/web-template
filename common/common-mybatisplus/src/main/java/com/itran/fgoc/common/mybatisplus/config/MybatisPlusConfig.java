65e3fbd945cc7fcfd49f14fc3c3a1f1daebf63c5
chun <1136389101@qq.com>
2022-06-06 23:53:18 +0800

    update: mybatisplus升级

--- common/common-mybatisplus/src/main/java/com/itran/fgoc/common/mybatisplus/config/MybatisPlusConfig.java
+++ common/common-mybatisplus/src/main/java/com/itran/fgoc/common/mybatisplus/config/MybatisPlusConfig.java
@@ -1,7 +1,10 @@
 package com.itran.fgoc.common.mybatisplus.config;
 
 
-import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.BlockAttackInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,13 +18,13 @@ import org.springframework.context.annotation.Configuration;
 @MapperScan(basePackages = {"com.itran.fgoc.**.mapper"})
 public class MybatisPlusConfig {
 
-
-	/**
-	 * 分页插件
-	 */
 	@Bean
-	public PaginationInterceptor paginationInterceptor() {
-		return new PaginationInterceptor();
+	public MybatisPlusInterceptor mybatisPlusInterceptor() {
+		MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+		// 分页插件
+		interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+		// 防全表更新插件
+		interceptor.addInnerInterceptor(new BlockAttackInnerInterceptor());
+		return interceptor;
 	}
-
 }
